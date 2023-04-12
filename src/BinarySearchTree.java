13186bf82d38ab593779eb48a99187b557de149d
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-04-12 09:29:31 -0400

    Changed the insert command with revisions then started to work on the contains method

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -6,62 +6,38 @@ public class BinarySearchTree {
         root = null;
     }
 
-    public void insert(Node<Album> current, Album album1) {
-        if (current.album.compareTo(album1) > 0) {
-            if (current.leftChild != null) {
-                insert(current.leftChild, album1);
+    public Node<Album> insert(Node<Album> current, Album album1) {
+        if(current != null) {
+            if (current.album.compareTo(album1) > 0)
+                current.leftChild = insert(current.leftChild, album1);
+  else if (current.album.compareTo(album1) < 0)
+                    current.rightChild = insert(current.rightChild, album1);
             } else {
-                current.leftChild = new Node<>(album1);
+                current = new Node<>(album1);
             }
-        } else if (current.album.compareTo(album1) < 0) {
-            if (current.rightChild != null) {
-                insert(current.rightChild, album1);
-            } else {
-                current.rightChild = new Node<>(album1);
-            }
-        }
+
+
+        return current;
     }
-    public Node<Album> insert(Album album1) {
-        if (root == null) {
-            root = new Node<>(album1);
+            public Node<Album> insert(Album album1){
+                this.root = insert(this.root, album1);
+                return this.root;
+            }
+    public Node<Album> contains (Node<Album> current, Album album1) {
+        if(current != null) {
+            if (current.album.compareTo(album1) > 0)
+                current.leftChild = insert(current.leftChild, album1);
+            else if (current.album.compareTo(album1) < 0)
+                current.rightChild = insert(current.rightChild, album1);
         } else {
-            insert(root, album1);
+            current = new Node<>(album1);
         }
-        return root;
+        return current;
     }
-    public Node<Album> deletePos(int delposition) {
-        IllegalArgumentException IAe = new IllegalArgumentException();
-        if (delposition < 0) {
-            throw IAe;
-        }
-        Node<Album> toDelete;
-        if (root == null) {
-            throw IAe;
-        } else if (delposition == 0) {
-            toDelete = root;
-            root = root.leftChild;
-            if (root != null) {
-                root.rightChild = null;
-            }
-        } else {
-            Node<Album> current = root;
-            int pos = 0;
-            while (pos < delposition - 1 && current.leftChild != null) {
-                current = current.rightChild;
-                pos++;
-            }
-            if (current.leftChild == null) {
-                throw IAe;
-            }
-            toDelete = current.rightChild;
-            current.leftChild = toDelete.rightChild;
-            if (toDelete.leftChild != null) {
-                current.leftChild.rightChild = current;
-            }
-        }
-        toDelete.leftChild = null;
-        toDelete.rightChild = null;
-        return toDelete;
+    public boolean contains(Album album1){
+        return contains(this.root, album1) != null;
+
     }
 
 }
+
